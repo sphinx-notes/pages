2fd32d4d58b4632529d468cbbcedb7e4db23c332
Shengyu Zhang <i@silverrainz.me>
2020-12-05 22:46:40 +0800

    Let it works
    I dont known why I need a JS launcher, if I use "composite run steps
    actions", I can not get input arguments from env variables.

--- main.sh
+++ main.sh
@@ -1,70 +1,95 @@
 #!/bin/bash
 
-set -x
+# set -x
+# set -e
 
-start_group(){
-    echo ::group::$@
-}
+repo_dir=$GITHUB_WORKSPACE/$INPUT_REPOSITORY_PATH
+doc_dir=$repo_dir/$INPUT_DOCUMENTATION_PATH
 
-end_group() {
-    echo ::endgroup::
-}
+echo ::group:: Initialize various paths
+echo Workspace: $GITHUB_WORKSPACE
+echo Repository: $repo_dir
+echo Documentation: $doc_dir
+echo ::endgroup::
 
-# The actions doesn't depends on any images, so we have to try various
-# package manager.
-start_group Installing Sphinx
-if [ -x "$(apt -v)" ]; then
+# The actions doesn't depends on any images,
+# so we have to try various package manager.
+echo ::group:: Installing Sphinx
+if command -v apt &>/dev/null; then
     # Debian/Ubuntu
-    apt update
-    apt install python3-sphinx python3-pip
-elif [ -x "$(zypper -v)" ]; then
+    sudo apt update
+    sudo apt install python3-sphinx python3-pip
+elif command -v zypper &>/dev/null; then
     # openSUSE
-    zypper update
-    zypper install python-Sphinx python3-pip
-elif [ -x "$(yum -v)" ]; then
+    sudo zypper update
+    sudo zypper install python-Sphinx python3-pip
+elif command -v yum &>/dev/null; then
     # RHEL, CentOS
-    yum update
-    yum install python-sphinx python-pip
-elif [ -x "$(pacman -v)" ]; then
-    pacman -Syy
-    pacman -S python-sphinx python-pip
-elif [ -x "$(brew -v)" ]; then
+    sudo yum update
+    sudo yum install python-sphinx python-pip
+elif command -v pacman &>/dev/null; then
+    # ArchLinux
+    sudo pacman -Syy
+    sudo pacman -S python-sphinx python-pip
+elif command -v brew &>/dev/null; then
+    # macOS
+    brew update
     brew install sphinx-doc
 fi
-end_group
-
-if [ "$INPUT_INSTALL_REQUIREMENTS" = true ] ; then
-    start_group Installing requirements
-    pip3 install -r $docs_src/$INPUT_SOURCE_DIR/requirements.txt
-    end_group
+echo Checking installation result
+if [ ! command -v sphinx-build &>/dev/null ] \
+    || [ ! command -v pip3 &>/dev/null ]; then
+    echo Sphinx or pip is not successfully installed
+    exit 1
+else
+    echo Everything goes well
 fi
+echo ::endgroup::
 
-start_group Creating temp directory
+echo ::group:: Installing requirements
+if [ "$INPUT_INSTALL_REQUIREMENTS" = "true" ] ; then
+    pip3 install -r $doc_dir/requirements.txt
+else
+    echo Skipped
+fi
+echo ::endgroup::
+
+echo ::group:: Creating temp directory
 tmp_dir=$(mktemp -d -t pages-XXXXXXXXXX)
-echo Temp directory $tmp_dir created
-endgroup
+echo Temp directory \"$tmp_dir\" is created
+echo ::endgroup::
 
-start_group Running Sphinx HTML builder
-sphinx-build -b html $INPUT_SOURCE_DIR $tmp_dir
-end_group
+echo ::group:: Running Sphinx builder
+sphinx-build -b html $doc_dir $tmp_dir
+echo ::endgroup::
 
-start_group Setting up branch $INPUT_TARGET_BRANCH
-branch_exist=$(git ls-remote --heads origin refs/heads/$INPUT_TARGET_BRANCH)
-if [ -z "$branch_exist" ]; then
-    echo Branch doesnt exist, create a emptry branch
-    git checkout --orphan $INPUT_TARGET_BRANCH
-else 
-    git checkout $INPUT_TARGET_BRANCH
-fi
-
-start_group Setting up git user information
+echo ::group:: Setting up git repository
+echo Setting up git configure
+cd $repo_dir
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
-endgroup
-
-start_group Committing HTML documentation
+echo Setting up branch $INPUT_TARGET_BRANCH
+branch_exist=$(git ls-remote --heads origin refs/heads/$INPUT_TARGET_BRANCH)
+if [ -z "$branch_exist" ]; then
+    echo Branch doesn\'t exist, create an emptry branch
+    git checkout --orphan $INPUT_TARGET_BRANCH
+    git rm --cached -r .
+else
+    echo Branch exists, chekcout to it
+    git checkout $INPUT_TARGET_BRANCH
+fi
+echo Cleanning up git workspace
 git clean -fd
-cp $tmp_dir/* .
-git commit -a --allow-empty -m "Add changes" 
-endgroup ::endgroup::
+echo ::endgroup::
 
+echo ::group:: Committing HTML documentation
+cd $repo_dir
+echo Deleting all file in repository
+rm -vrf *
+echo Copying HTML documentation to repository
+cp -vr $tmp_dir/. .
+echo Adding HTML documentation to repository index
+git add .
+echo Recording changes to repository
+git commit --allow-empty -m "Add changes"
+echo ::endgroup::
