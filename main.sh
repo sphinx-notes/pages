c82d384816d5a203a9bc4ca1c586aff62106bedc
Shengyu Zhang <i@silverrainz.me>
2022-11-30 22:04:10 +0800

    Restore timestamp of git repository

--- main.sh
+++ main.sh
@@ -5,8 +5,11 @@ set -e
 
 repo_dir=$GITHUB_WORKSPACE/$INPUT_REPOSITORY_PATH
 doc_dir=$repo_dir/$INPUT_DOCUMENTATION_PATH
+# https://stackoverflow.com/a/4774063/4799273
+action_dir="$(cd -- "$(dirname "$-1")" >/dev/null 2>&1 ; pwd -P)"
 
 echo ::group:: Initialize various paths
+echo Action: $action_dir
 echo Workspace: $GITHUB_WORKSPACE
 echo Repository: $repo_dir
 echo Documentation: $doc_dir
@@ -45,6 +48,8 @@ if [ ! -z "$INPUT_REQUIREMENTS_PATH" ] ; then
     echo ::endgroup::
 fi
 
+echo ::group:: Preparations for incremental build
+
 # Sphinx HTML builder will rebuild the whole project when modification time
  # (mtime) of templates of theme newer than built result. [1]
 #
@@ -53,13 +58,18 @@ fi
 # Set mtime to 1990 to make sure the project won't rebuilt.
 #
 # .. [1] https://github.com/sphinx-doc/sphinx/blob/5.x/sphinx/builders/html/__init__.py#L417
-echo ::group:: Fixing timestamp of HTML theme
+echo Fixing timestamp of HTML theme
 site_packages_dir=$(python -c 'import site; print(site.getsitepackages()[0])')
 echo Python site-packages directory: $site_packages_dir
 for i in $(find $site_packages_dir -name '*.html'); do
     touch -m -t 190001010000 $i
     echo Fixing timestamp of $i
 done
+
+echo Restoring timestamp of git repository
+git_restore_mtime=$action_dir/git-restore-mtime
+$git_restore_mtime $repo_dir
+
 echo ::endgroup::
 
 echo ::group:: Creating build directory
