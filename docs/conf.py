b3f627531947d7da5d672408abbc9a9329cc25c5
Shengyu Zhang <i@silverrainz.me>
2023-02-10 21:37:45 +0800

    Re-org docs with https://github.com/sphinx-notes/template

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,36 @@
+# This file is generated from sphinx-notes/template. DO NOT EDIT.
+#
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
 
 # -- Project information -----------------------------------------------------
 
 project = 'sphinxnotes-pages'
-copyright = '2020, Shengyu Zhang'
 author = 'Shengyu Zhang'
+copyright = "2023, " + author
 
 # The full version, including alpha/beta/rc tags
-release = '1.0a'
-
+version = release = 'v3'
 
 # -- General configuration ---------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('../src/sphinxnotes'))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.githubpages',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -45,25 +45,31 @@ master_doc = 'index'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# A boolean that decides whether codeauthor and sectionauthor directives
+# produce any output in the built files.
+show_authors = True
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
 
-html_theme_options = {
-    'nosidebar': True,
-}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {
+    "source_repository": "https://github.com/sphinx-notes/pages/",
+    "source_branch": "master",
+    "source_directory": "docs/",
+}
 
 # The URL which points to the root of the HTML documentation.
 # It is used to indicate the location of document like canonical_url
-html_baseurl = 'https://sphinx-notes.github.io/pages'
+html_baseurl = 'https://sphinx.silverrainz.me/pages'
 
 html_logo = html_favicon = '_images/sphinx-notes.png'
--- docs/index.rst
+++ docs/index.rst
@@ -1,1 +1,18 @@
+.. This file is generated from sphinx-notes/template. DO NOT EDIT.
+
 .. include:: ../README.rst
+
+The Sphinx Notes Project
+========================
+
+This project is a developed by `Shengyu Zhang`__,
+as part of **The Sphinx Notes Project**.
+
+.. toctree::
+   :caption: The Sphinx Notes Project
+
+   Home <https://sphinx.silverrainz.me/>
+   Blog <https://silverrainz.me/blog/category/sphinx.html>
+   PyPI <https://pypi.org/search/?q=sphinxnotes>
+
+__ https://github.com/SilverRainZ
